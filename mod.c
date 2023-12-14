d30add8731fffac45ca64fba3ea938cce98d0c57
MmeliNtshakala <mmeliroot@gmail.com>
2023-12-14 15:49:07 -0800

    mod

--- mod.c
+++ mod.c
@@ -12,3 +12,35 @@ void f_mod(stack_t **head, unsigned int line_number)
 {
 	stack_t *original_head;
 	unsigned int len = 0, temp;
+
+	original_head = *head;
+
+
+	while (original_head != NULL)
+	{
+		len++;
+		original_head = original_head->next;
+	}
+	if (len < 2)
+	{
+		fflush(stdout);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		fclose(global.file);
+		free(global.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*head)->n == 0)
+	{
+		fflush(stdout);
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*head)->next->n % (*head)->n;
+	(*head)->next->n = temp;
+	original_head = *head;
+	(*head) = (*head)->next;
+	free(original_head);
+}
